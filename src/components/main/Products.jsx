206184ccc0bb363bf5da6e9cd24e41d2c0316cd4
Akshat <IIT2022046@iiita.ac.in>
2026-06-07 11:21:47 +0530

    Completed Products Page

--- src/components/main/Products.jsx
+++ src/components/main/Products.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
+import ProductCard from "../ProductCard";
+
 function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        // Remove unwanted product IDs
+        const filteredProducts = data.products.filter(
+          (product) =>
+            product.id !== 17 &&
+            product.id !== 19 &&
+            product.id !== 23 &&
+            product.id !== 24,
+        );
+
+        setProducts(filteredProducts);
+      });
+  }, []);
+
   return (
-    <>
-      Welcome to the Products page! Here you can browse through our wide
-      selection of products and find the perfect items to suit your needs. Happy
-      shopping!
-    </>
+    <div className="d-flex justify-content-center flex-wrap gap-4 my-4 px-3">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
   );
 }
+
 export default Products;
